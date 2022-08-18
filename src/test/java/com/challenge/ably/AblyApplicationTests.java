adcea5e732dc2220578202c0ddc695b81932ddde
양준렬 <jlyang@deleo.co.kr>
2022-08-18 18:31:43 +0900

    A company hiring project
    - Project Setting

---
+++ src/test/java/com/challenge/ably/AblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ably;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AblyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
