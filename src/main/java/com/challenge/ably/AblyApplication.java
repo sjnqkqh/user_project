68ecb070042d294ef6a30043148524edd70570f3
양준렬 <jlyang@deleo.co.kr>
2022-08-18 18:45:52 +0900

    A company hiring project
    - Project Setting

--- src/main/java/com/challenge/ably/AblyApplication.java
+++ src/main/java/com/challenge/ably/AblyApplication.java
@@ -2,14 +2,12 @@ package com.challenge.ably;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class AblyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AblyApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AblyApplication.class, args);
+	}
 
 }
