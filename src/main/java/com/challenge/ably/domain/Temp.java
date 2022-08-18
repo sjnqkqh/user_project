0fdf1af9502210f17eb7bf9d2481b331c395a97f
양준렬 <jlyang@deleo.co.kr>
2022-08-18 18:45:52 +0900

    A company hiring project
    - Project Setting

---
+++ src/main/java/com/challenge/ably/domain/Temp.java
@@ -0,0 +1,22 @@
+package com.challenge.ably.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+
+@Entity(name = "temp")
+@Getter
+public class Temp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+}
