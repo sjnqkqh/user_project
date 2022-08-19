1715b3d26fbd89ed8e79e49a097a4691425873d6
양준렬 <jlyang@deleo.co.kr>
2022-08-19 10:58:22 +0900

    A company hiring project
    - Add exception handling classes

---
+++ src/main/java/com/challenge/ably/dto/CommonRespDto.java
@@ -0,0 +1,12 @@
+package com.challenge.ably.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+@AllArgsConstructor
+public class CommonRespDto {
+
+    private Boolean result;
+}
