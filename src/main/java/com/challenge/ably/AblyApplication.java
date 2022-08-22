50bfed7c1bd6d979364b988e7119ff29cdcbedd2
양준렬 <jlyang@deleo.co.kr>
2022-08-22 17:39:31 +0900

    채용 과제 프로젝트 커밋

--- src/main/java/com/challenge/ably/AblyApplication.java
+++ src/main/java/com/challenge/ably/AblyApplication.java
@@ -2,12 +2,14 @@ package com.challenge.ably;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AblyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AblyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AblyApplication.class, args);
+    }
 
 }
