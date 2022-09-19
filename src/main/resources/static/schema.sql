e365f9377a4ba369fc4673b1e125ceb62ff6f5e0
양준렬 <jlyang@deleo.co.kr>
2022-09-19 14:59:03 +0900

    init query 옵션 제거
    - 사용하던 데이터베이스를 H2에서 Mysql로 변경함에 따라 불필요해진 옵션 제거

--- src/main/resources/static/schema.sql
+++ src/main/resources/static/schema.sql
@@ -1,22 +1,23 @@
 DROP TABLE IF EXISTS TB_USER_TOKEN;
 DROP TABLE IF EXISTS TB_PHONE_AUTH;
-DROP TABLE IF EXISTS TB_USER;
+DROP TABLE IF EXISTS TB_BOARD_FILE;
 DROP TABLE IF EXISTS TB_BOARD;
+DROP TABLE IF EXISTS TB_USER;
 
 
 /*======================================================================================================================================*/
 
 CREATE TABLE TB_USER
 (
-    user_id      BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    login_id     VARCHAR(100) NOT NULL,
-    email        VARCHAR(100) NOT NULL,
-    enc_password VARCHAR(100) NOT NULL,
-    nickname     VARCHAR(40)  NOT NULL,
-    enc_phone    VARCHAR(50)  NOT NULL,
-    delete_yn    VARCHAR(1)   NOT NULL DEFAULT 'N',
-    created_at   DATETIME     NOT NULL DEFAULT NOW(),
-    updated_at   DATETIME     NOT NULL DEFAULT NOW()
+    USER_ID      BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    LOGIN_ID     VARCHAR(100) NOT NULL,
+    EMAIL        VARCHAR(100) NOT NULL,
+    ENC_PASSWORD VARCHAR(100) NOT NULL,
+    NICKNAME     VARCHAR(40)  NOT NULL,
+    ENC_PHONE    VARCHAR(50)  NOT NULL,
+    DELETE_YN    VARCHAR(1)   NOT NULL DEFAULT 'N',
+    CREATED_AT   DATETIME     NOT NULL DEFAULT NOW(),
+    UPDATED_AT   DATETIME     NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX EMAIL_IDX ON TB_USER (login_id);
@@ -25,33 +26,36 @@ CREATE INDEX EMAIL_IDX ON TB_USER (login_id);
 
 CREATE TABLE TB_USER_TOKEN
 (
-    token_id                 BIGINT PRIMARY KEY AUTO_INCREMENT,
-    user_id                  BIGINT              NOT NULL REFERENCES TB_USER (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    access_token             VARCHAR(500) UNIQUE NOT NULL,
-    refresh_token            VARCHAR(500) UNIQUE NOT NULL,
-    refresh_token_expired_at DATETIME            NOT NULL DEFAULT NOW(),
-    created_at               DATETIME            NOT NULL DEFAULT NOW(),
-    updated_at               DATETIME            NOT NULL DEFAULT NOW()
+    TOKEN_ID                 BIGINT PRIMARY KEY AUTO_INCREMENT,
+    USER_ID                  BIGINT              NOT NULL REFERENCES TB_USER (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ACCESS_TOKEN             VARCHAR(500) UNIQUE NOT NULL,
+    REFRESH_TOKEN            VARCHAR(500) UNIQUE NOT NULL,
+    REFRESH_TOKEN_EXPIRED_AT DATETIME            NOT NULL DEFAULT NOW(),
+    CREATED_AT               DATETIME            NOT NULL DEFAULT NOW(),
+    UPDATED_AT               DATETIME            NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX ACCESS_TOKEN_IDX on TB_USER_TOKEN (access_token);
+
+insert into tb_user (user_id, created_at, updated_at, delete_yn, email, enc_password, enc_phone, login_id, nickname)
+values (default, default, default, 'N', 'test@hiring.co.kr', '$2a$10$34Exz.d/pt1LHB/30jQazuRYQpuhSpT7ihmiL1SOT3t9APv6dVSMK', 'k8VwutUqQwM0PPiGCtHjVg', NOW(), NOW());
 
 /*======================================================================================================================================*/
 
 CREATE TABLE TB_PHONE_AUTH
 (
-    auth_id         BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id         BIGINT       NULL REFERENCES TB_USER (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    enc_phone       VARCHAR(50)  NOT NULL,
-    telecom_code    ENUM ('SKT','KT','LGU','SAVE_SKT','SAVE_KT','SAVE_LGU'),
-    auth_value      VARCHAR(20)  NOT NULL,
-    authentication  VARCHAR(200) NULL,
+    AUTH_ID         BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    USER_ID         BIGINT       NULL REFERENCES TB_USER (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    ENC_PHONE       VARCHAR(50)  NOT NULL,
+    TELECOM_CODE    ENUM ('SKT','KT','LGU','SAVE_SKT','SAVE_KT','SAVE_LGU'),
+    AUTH_VALUE      VARCHAR(20)  NOT NULL,
+    AUTHENTICATION  VARCHAR(200) NULL,
     AUTHORIZED_YN   CHAR(1)      NOT NULL DEFAULT 'N',
-    auth_type       ENUM ('SIGN_IN', 'PASSWORD_RESET'),
-    auth_until      DATETIME     NOT NULL DEFAULT NOW(),
-    guarantee_until DATETIME     NULL,
-    created_at      DATETIME     NOT NULL DEFAULT NOW(),
-    updated_at      DATETIME     NOT NULL DEFAULT NOW()
+    AUTH_TYPE       ENUM ('SIGN_IN', 'PASSWORD_RESET'),
+    AUTH_UNTIL      DATETIME     NOT NULL DEFAULT NOW(),
+    GUARANTEE_UNTIL DATETIME     NULL,
+    CREATED_AT      DATETIME     NOT NULL DEFAULT NOW(),
+    UPDATED_AT      DATETIME     NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX PHONE_INFO_IDX ON TB_PHONE_AUTH (enc_phone, telecom_code, auth_type);
@@ -61,13 +65,24 @@ CREATE INDEX AUTHENTICATION_IDX ON TB_PHONE_AUTH (auth_type, authentication);
 
 CREATE TABLE TB_BOARD
 (
-    id         bigint unsigned NOT NULL AUTO_INCREMENT,
-    user_id    bigint       NOT NULL,
-    title      varchar(255) NOT NULL,
-    content    text         NOT NULL,
-    created_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    KEY        fk_board_to_user_idx (user_id),
-    KEY        fk_board_to_user (user_id)
+    POST_ID    BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    USER_ID    BIGINT UNSIGNED NOT NULL,
+    TITLE      VARCHAR(255)    NOT NULL,
+    CONTENT    TEXT            NOT NULL,
+    CREATED_AT DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UPDATED_AT DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (USER_ID) REFERENCES TB_USER (USER_ID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+/*======================================================================================================================================*/
+
+CREATE TABLE TB_BOARD_FILE
+(
+    POST_FILE_ID      BIGINT UNSIGNED PRIMARY KEY,
+    POST_ID           BIGINT UNSIGNED NOT NULL REFERENCES TB_BOARD (POST_ID) ON UPDATE CASCADE ON DELETE CASCADE,
+    ORIGIN_FILE_NAME  VARCHAR(255)    NOT NULL,
+    RESTORE_FILE_NAME VARCHAR(255)    NOT NULL,
+    CREATED_AT        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UPDATED_AT        DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
